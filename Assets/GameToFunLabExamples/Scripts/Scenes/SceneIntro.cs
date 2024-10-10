77e38f2ed34aeb447e999485db9b537b439dea8e
weeperzzang <weeperzzang@naver.com>
2024-10-10 12:20:59 +0900

    예제 씬 추가 - 인트로, 데이터 테이블 로딩 씬

--- Assets/GameToFunLabExamples/Scripts/Scenes/SceneIntro.cs
+++ Assets/GameToFunLabExamples/Scripts/Scenes/SceneIntro.cs
@@ -1,9 +1,13 @@
-﻿using GameToFunLab.Scenes;
+﻿using GameToFunLab.Core;
+using GameToFunLab.Scenes;
 
 namespace Scripts.Scenes
 {
     public class SceneIntro : DefaultScene
     {
-        
+        public void OnClickGameStart()
+        {
+            SceneManager.LoadSceneByName("Loading");
+        }
     }
 }
